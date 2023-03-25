7ec2c46b8388eec3831eab6bdfa264a2f7eb87f2
Balázs Szi-Benedek <g5644h@INF.ELTE.HU>
2023-03-25 23:47:22 +0100

    Implemented FieldItem uml

--- CCity/ViewModel/FieldItem.cs
+++ CCity/ViewModel/FieldItem.cs
@@ -1,35 +1,89 @@
-﻿using System;
+﻿using CCity.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace CCity.ViewModel
 {
-    public class FieldItem
+    public class FieldItem :ViewModelBase
     {
 
         #region Fields
 
-
+        private int _level;
+        private Texture _texture;
+        private Color _minimapColor;
+        private Color _overlayColor;
 
         #endregion
 
         #region Properties
 
+        public int Level {
+            get { return _level; }
+            set
+            {
+                if(_level != value)
+                {
+                    _level = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
+        public Texture Texture {
+            get { return _texture; }
+            set
+            {
+                if(_texture != value)
+                {
+                    _texture = value;
+                    OnPropertyChanged();
+                }
+            } 
+        }
 
-        #endregion
+        public Color MinimapColor { 
+            get { return _minimapColor; }
+            set
+            {
+                if(_minimapColor != value)
+                {
+                    _minimapColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
-        #region Constructors
+        public Color OverLayColor {
+            get { return _overlayColor; }
+            set
+            {
+                if(_overlayColor != value)
+                {
+                    _overlayColor = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
 
+        public int X { get; }
+        public int Y { get; }
+        public int Number { get; }
 
+        private 
 
         #endregion
 
         #region Public methods
 
-
+        DelegateCommand ClickCommand()
+        {
+            throw new NotImplementedException();
+        }
 
         #endregion
 
