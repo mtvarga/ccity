1a9c04a6ca4a2ef1d37d341621a9ce64aefa7536
Martin Solti <beszn1@inf.elte.hu>
2023-03-28 20:23:25 +0200

    Improved Field

--- CCity.Model/Field.cs
+++ CCity.Model/Field.cs
@@ -8,13 +8,22 @@ namespace CCity.Model
 {
     public class Field
     {
-        #region Properties
+        #region Fields
 
-        public Placeable? Placeable { get; internal set; }
-        public int LastCalculatedSatisfaction { get; internal set; }
-        public double ForestEffect { get; internal set; }
-        public double FireDepartmentEffect { get; internal set; }
+        public Placeable Placeable { get; internal set; }
+        
+        //to int
+        public double LastCalculatedSatisfaction { get; internal set; }
+
+        private int _policeDepartmentEffect;
+        private int _stadiumEffect;
+        private int _fireDepartmentEffect;
+        private int _forestEffect;
+
         public double PoliceDepartmentEffect { get; internal set; }
+        public double StadiumEffect { get; internal set; }
+        public double FireDepartmentEffect { get; internal set; }
+        public double ForestEffect { get; internal set; }
 
         #endregion
 
@@ -22,7 +31,12 @@ namespace CCity.Model
 
         public Field()
         {
-            throw new NotImplementedException();
+            Placeable = null;
+            LastCalculatedSatisfaction = 0;
+            PoliceDepartmentEffect = 0;
+            StadiumEffect = 0;
+            FireDepartmentEffect = 0;
+            ForestEffect = 0;
         }
 
         #endregion
@@ -31,7 +45,21 @@ namespace CCity.Model
 
         public int CalculateSatisfaction()
         {
-            throw new NotImplementedException();
+            if (Placeable == null || !(Placeable is Zone))
+            {
+                LastCalculatedSatisfaction = 0;
+                return 0;
+            }
+
+            Zone zone = Placeable as Zone;
+            double tempSatisfaction = 0;
+            foreach(Citizen citizen in zone.Citizens)
+            {
+                tempSatisfaction += citizen.Satisfaction;
+            }
+
+            LastCalculatedSatisfaction = tempSatisfaction;
+            return 0;
         }
 
         #endregion
