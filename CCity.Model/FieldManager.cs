2745a2a0aa3f0576399d47953767cc8204401e88
Balázs Szi-Benedek <g5644h@INF.ELTE.HU>
2023-03-31 16:12:12 +0200

    [FieldManager] Updated road related methods

--- CCity.Model/FieldManager.cs
+++ CCity.Model/FieldManager.cs
@@ -2,8 +2,6 @@
 {
     public class FieldManager
     {
-        //commit test
-
         #region Constants
 
         private const int MAX_EFFECT = 20;
@@ -92,21 +90,6 @@
             throw new NotImplementedException();
         }
 
-        private void HandleRoadDemolition(Field field)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void HandleRoadPlacement(Field field)
-        {
-            throw new NotImplementedException();
-        }
-
-        private bool CanDemolishRoad(Field field)
-        {
-            throw new NotImplementedException();
-        }
-
         private bool CanPlace(int x, int y, Placeable placeable)
         {
             if (!OnMap(x, y))
@@ -169,6 +152,81 @@
             }
             return effectedFields;
         }
+
+        private bool CanDemolishRoad(Field field)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleRoadPlacement(Field field)
+        {
+            Road placedRoad = (Road)field.Placeable;
+            List<Field> neigbours = GetRoadNeighbours(field.X, field.Y);
+
+            foreach (Field neigbour in neigbours)
+            {
+                Road road = (Road)neigbour.Placeable;
+                if (road.IsPublic)
+                {
+                    placedRoad.GetPublicityFrom = road;
+                    road.GivesPublicityTo.Add(placedRoad);
+                    neigbours.Remove(neigbour);
+                    break;
+                }
+            }
+
+            if (placedRoad.IsPublic)
+            {
+                SpreadPublicity(placedRoad, neigbours);
+            }
+
+        }
+
+        private void HandleRoadDemolition(Field field)
+        {
+            Road demolishedRoad = (Road)field.Placeable;
+            if (demolishedRoad.IsPublic)
+            {
+                foreach (Road road in demolishedRoad.GivesPublicityTo)
+                {
+                    ModifyRoad(road);
+                }
+
+            }
+        }
+
+        private List<Field> GetRoadNeighbours(int x, int y)
+        {
+            List<Field> neighbours = new List<Field>();
+
+            if (OnMap(x, y) && Fields[x - 1][y].Placeable is Road) neighbours.Add(Fields[x - 1][y]);
+            if (OnMap(x, y) && Fields[x + 1][y].Placeable is Road) neighbours.Add(Fields[x + 1][y]);
+            if (OnMap(x, y) && Fields[x][y - 1].Placeable is Road) neighbours.Add(Fields[x][y - 1]);
+            if (OnMap(x, y) && Fields[x][y + 1].Placeable is Road) neighbours.Add(Fields[x][y + 1]);
+
+            return neighbours;
+        }
+
+        private void SpreadPublicity(Road actualRoad, List<Field> neigbours)
+        {
+            if (neigbours.Count == 0) return;
+            foreach (Field neigbour in neigbours)
+            {
+                Road road = (Road)neigbour.Placeable;
+                if (!road.IsPublic)
+                {
+                    road.GetPublicityFrom = actualRoad;
+                    actualRoad.GivesPublicityTo.Add(road);
+                    SpreadPublicity(road, GetRoadNeighbours(neigbour.X, neigbour.Y));
+                };
+            }
+        }
+
+        private void ModifyRoad(Road road)
+        {
+
+        }
+
 
         #endregion
     }
