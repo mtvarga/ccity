95a0f1986dcc23b078acc50fd73ae7666cb2866d
Martin Solti <beszn1@inf.elte.hu>
2023-03-31 15:55:18 +0200

    Updated Field and FieldManager

--- CCity.Model/Field.cs
+++ CCity.Model/Field.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CCity.Model
+﻿namespace CCity.Model
 {
     public class Field
     {
