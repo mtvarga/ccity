a52ff686dcf99df8c9c3960bcc8cf6269eb95d48
Martin Solti <beszn1@inf.elte.hu>
2023-04-08 16:55:24 +0200

    [View] Added StartupWindow with some basic temp style, added NavigateTo method, updated App

--- CCity/App.xaml.cs
+++ CCity/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using CCity.Model;
+using CCity.View;
+using CCity.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace CCity
 {
@@ -13,5 +17,27 @@ namespace CCity
     /// </summary>
     public partial class App : Application
     {
+        //private MainModel _model;
+        //private MainViewModel _viewModel;
+        private MainWindow _mainWindow;
+        private UserControl _startupWindow;
+
+        App()
+        {
+            Startup += new StartupEventHandler(App_Startup);
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            //_model = new();
+            //_viewModel = new(_model);
+            _mainWindow = new();
+            _startupWindow = new StartupWindow();
+
+            //_mainWindow.DataContext = _viewModel;
+            _mainWindow.NavigateTo(_startupWindow);
+            _mainWindow.Show();
+
+        }
     }
 }
