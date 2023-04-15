462f1970af170e5ebd506f89490cec78440f7897
Balázs Szi-Benedek <g5644h@INF.ELTE.HU>
2023-04-15 18:53:55 +0200

    [App] implemented App constructor and methods

--- CCity/App.xaml.cs
+++ CCity/App.xaml.cs
@@ -3,12 +3,14 @@ using CCity.View;
 using CCity.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace CCity
 {
@@ -17,31 +19,123 @@ namespace CCity
     /// </summary>
     public partial class App : Application
     {
-        //private MainModel _model;
-        //private MainViewModel _viewModel;
+        #region Fields
+
+        private MainModel _model;
+        private MainViewModel _viewModel;
         private MainWindow _mainWindow;
         private UserControl _startupWindow;
         private UserControl _gameWindow;
+        private DispatcherTimer _timer;
+
+        #endregion
+
+        #region Constructor
 
         App()
         {
             Startup += new StartupEventHandler(App_Startup);
         }
 
+        #endregion
+
+        #region Application event handler
+
         private void App_Startup(object sender, StartupEventArgs e)
         {
-            //_model = new();
-            //_viewModel = new(_model);
+            _model = new();
+            _model.GameOver += new EventHandler<EventArgs>(Model_GameOver);
+
+            _viewModel = new(_model);
+            _viewModel.NewGame += new EventHandler(ViewModel_NewGame);
+            _viewModel.PauseGame += new EventHandler(ViewModel_PauseGame);
+            _viewModel.ExitGame += new EventHandler(ViewModel_ExitGame);
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(250);
+            _timer.Tick += new EventHandler(Time_Ticked);
+
             _mainWindow = new();
+            _mainWindow.Closing += new System.ComponentModel.CancelEventHandler(View_Closing);
+
+
             _startupWindow = new StartupWindow();
             _gameWindow = new GameWindow();
 
-            //_mainWindow.DataContext = _viewModel;
+
+            _mainWindow.DataContext = _viewModel;
 
             //_mainWindow.NavigateTo(_startupWindow);
             _mainWindow.NavigateTo(_gameWindow); //for testing purposes
             _mainWindow.Show();
+        }
+
+        private void Time_Ticked(object? value, EventArgs eventArgs)
+        {
+            _model.TimerTick();
+        }
+
+        #endregion
+
+        #region View event handlers
+
+        //TODO
+        private void View_Closing(object? sender, CancelEventArgs e)
+        {
+            bool restartTimer = _timer.IsEnabled;
+            _timer.Stop();
+            if (MessageBox.Show("Biztos be akarod zárni a játékot? Az aktuális jákékmenet elfog veszni.", "C City", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            {
+                e.Cancel=true;
+                _timer.Start();
+
+                if(restartTimer)
+                {
+                    _timer.Start();
+                }
+            }
 
         }
+
+        #endregion
+
+        #region ViewModel event handlers
+
+        private void ViewModel_NewGame(object? sender, EventArgs e)
+        {
+            _mainWindow.NavigateTo(_gameWindow);
+            _timer.Start();
+        }
+
+        private void ViewModel_ExitGame(object? sender, EventArgs e)
+        {
+            _mainWindow.NavigateTo(_startupWindow);
+        }
+
+        private void ViewModel_PauseGame(object? sender, EventArgs e)
+        {
+            if (_viewModel.Paused)
+            {
+                _timer.Start();
+            }
+            else
+            {
+                _timer.Stop();
+            }
+        }
+
+        #endregion
+
+        #region Model event handlers
+
+        private void Model_GameOver(object? sender, EventArgs e)
+        {
+            _timer.Stop();
+
+            MessageBox.Show("Vége a játéknak, túl sok elégedetlen polgár volt!", "C City", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+        }
+
+        #endregion
+
     }
 }
