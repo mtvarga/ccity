79ffcd328001f8c6d2cfe83a57675f970d9a229c
Varga Máté Tibor <vv8wb3@inf.elte.hu>
2023-04-16 13:52:07 +0000

    Implemented CitizenManager

--- CCity.Model/GlobalManager.cs
+++ CCity.Model/GlobalManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,6 @@ namespace CCity.Model
 {
     public class GlobalManager
     {
-
         #region Fields
 
         public int GlobalSatisfactionScore;
