70e2e92e5af6811d111304f77b9f0920390de61c
Martin Solti <beszn1@inf.elte.hu>
2023-04-17 17:11:35 +0200

    [View] Added textures and triggers ( :'( ), value converter currently is out of service

--- CCity/ViewModel/TextureEnumToPath.cs
+++ CCity/ViewModel/TextureEnumToPath.cs
@@ -8,6 +8,7 @@ using System.Windows.Data;
 
 namespace CCity.ViewModel
 {
+    [ValueConversion(typeof(Texture), typeof(string))]
     public class TextureEnumToPath : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -16,12 +17,13 @@ namespace CCity.ViewModel
             {
                 switch (texture)
                 {
-                    case Texture.PoliceDepartment:
-                        return "Images/Textures/policeDepartment.png";
+                    case Texture.PoliceDepartment: return "Images/Textures/policeDepartment.png";
+                    case Texture.FireDepartment: return "Images/Textures/fireDepartment.png";
+                    default: return "Images/Textures/transparent.png";
                 }
             }
 
-            return null;
+            return "Images/Textures/transparent.png";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
