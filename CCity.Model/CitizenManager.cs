1557ca432d70c35a7e627fa92e8aad920b9390c7
Máté Varga <vv8wb3@inf.elte.hu>
2023-04-18 12:55:52 +0200

    [CitizenManager] Added jobless citizens list

--- CCity.Model/CitizenManager.cs
+++ CCity.Model/CitizenManager.cs
@@ -20,6 +20,8 @@
         
         public int Population => Citizens.Count;
 
+        private List<Citizen> JoblessCitizens { get; }
+        
         #endregion
 
         #region Constructors
@@ -27,6 +29,8 @@
         public CitizenManager()
         {
             Citizens = new List<Citizen>();
+            JoblessCitizens = new List<Citizen>();
+            
             _nextToCommercial = true;
         }
 
